ac5605ab22b2f635b3ab32b1cdfed72087f2e05c
Franco Mayer <Francomayer96@outlook.com>
2023-10-02 15:47:37 -0300

    Agregando mas funciones y estilos

--- js/calculator.js
+++ js/calculator.js
@@ -6,9 +6,36 @@ function clearDisplay() {
     document.getElementById('display').value = '';
 }
 
+
 function calculate() {
+    var expression = document.getElementById('display').value;
     try {
-        document.getElementById('display').value = eval(document.getElementById('display').value);
+        var result = eval(expression); 
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
+function addDecimal() {
+    var currentDisplay = document.getElementById('display').value;
+
+    if (currentDisplay.indexOf('.') === -1) {
+        document.getElementById('display').value += '.';
+    }
+}
+
+function backspace() {
+    var currentDisplay = document.getElementById('display').value;
+    document.getElementById('display').value = currentDisplay.slice(0, -1);
+}
+
+function calculatePercentage() {
+    var expression = document.getElementById('display').value;
+    try {
+        var result = eval(expression); 
+        result = (result / 100).toFixed(2); 
+        document.getElementById('display').value = result;
     } catch (error) {
         document.getElementById('display').value = 'Error';
     }
